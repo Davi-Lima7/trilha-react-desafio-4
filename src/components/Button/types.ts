57f7c5b27a9397e7c838ecb3bebfa43dea8b9486
Davi <davilimadealmeida60@gmail.com>
2025-01-28 11:53:44 -0300

    Desafio Concluido

--- src/components/Button/types.ts
+++ src/components/Button/types.ts
@@ -1,4 +1,11 @@
 export interface IButtonProps {
-    title: string;
-    onClick?: React.MouseEventHandler<HTMLButtonElement>;
+  title: string;
+  disabled?: boolean;
+  onClick?: React.MouseEventHandler<HTMLButtonElement>;
+}
+
+export interface IButtonPropsStyled {
+  onClick?: React.MouseEventHandler<HTMLButtonElement>;
+  children?: React.ReactNode;
+  disabled?: boolean;
 }
--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -21,12 +21,17 @@ const Login = () => {
   const {
     control,
     formState: { errors, isValid },
+    handleSubmit,
   } = useForm<IFormLogin>({
     resolver: yupResolver(schema),
     mode: "onBlur",
     defaultValues,
     reValidateMode: "onChange",
   });
+
+  const onSubmit = () => {
+    alert("Login com sucesso!");
+  };
 
   return (
     <Container>
@@ -49,7 +54,7 @@ const Login = () => {
             errorMessage={errors?.password?.message}
           />
           <Spacing />
-          <Button title="Entrar" />
+          <Button title="Entrar" onClick={handleSubmit(onSubmit)} disabled={!isValid} />
         </Column>
       </LoginContainer>
     </Container>
